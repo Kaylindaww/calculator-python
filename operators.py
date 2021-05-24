c5dd52aa8f6a11da932c022c16a8449872b8bfe7
Kaylindaww <79210939+Kaylindaww@users.noreply.github.com>
2021-05-24 13:21:15 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,23 @@
+num1=int(input("enter first number;"))
+operator=input("enter operator;")
+num2=int(input("enter second number;"))
+# if operator is (+|-|/|*|%|)then print out number 1(+|-|*|/|%)number 2
+if operator =="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2) 
+elif operator=="%":
+    print(num1%num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="/":
+    print(num1/num2)
+else:
+    print("NOT VALID OPERATOR")
+
+    
+        
+                    
+                
+            
+        
